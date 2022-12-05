214fa310c6e61f6548406155664fb59c8a50a80c
DariusPorthos <darius.porthos@gmail.com>
2022-12-05 13:42:20 +0100

    test

--- View/view_begin_membre.php
+++ View/view_begin_membre.php
@@ -7,5 +7,14 @@
 </head>
 <body>
 <header>
-
+<nav>
+    <ul>
+        <li><img src="../Content/img/Logo_BDE_noir.png"></li>
+        <li><a href="?controller=list&action=last">Accueuil</a></li>
+        <li><a href="?controller=commande&action=commande">Commande</a></li>
+        <li><a href="?controller=list&action=last">Inventaire</a></li>
+        <li><a href="?controller=list&action=last">Comptabilite</a></li>
+        <li><a href="?controller=list&action=last"Gestion Comptes</li>
+    </ul>
+</nav>
 </header>
