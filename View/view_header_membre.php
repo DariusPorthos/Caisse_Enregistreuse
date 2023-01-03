ad012f9ade1e4216972a1adc8c4c5f4bba1a7ea3
DariusPorthos <119850378+DariusPorthos@users.noreply.github.com>
2023-01-03 10:42:19 +0100

    Add files via upload

--- View/view_header_membre.php
+++ View/view_header_membre.php
@@ -3,35 +3,60 @@
 <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="../Content/CSS/header.css">
+    <link rel="shortcut icon" href="../Content/Images/Logo_bde">
     <title>BDE USPN</title>
 </head>
 <body>
-<!-- Début header -->
+
 <header>
     <div class="container">
         <!-- Logo -->
         <div class="logo">
-            <a href=""><img src="../Images/Logo_bde" alt="Logo BDE"></a>
+            <a href=""><img src="../Content/Images/Logo_bde" alt="Logo BDE"></a>
         </div>
         <!-- Barre de navigation -->
         <nav>
             <ul class="navigation">
                 <!-- Côté gauche de la barre de navigation -->
                 <ul class="menuL">
-                    <li><a href="">Acceuil</a></li>
-                    <li><a href="">Commande</a></li>
-                    <li><a href="">Inventaire</a></li>
-                    <li><a href="">Comptabilité</a></li>
-                    <li><a href="">Gestion des comptes</a></li>
+                    <li style="padding-bottom: 2.6%">
+                        <a href="">Accueil</a>
+                    </li>
+                    <li>
+                        <a href="">Commande</a>
+                        <!-- Sous menu de commande -->
+                        <ul>
+                            <li><a href="">Nouvelle Commande</a></li>
+                            <li><a href="">Retour Commande</a></li>
+                        </ul>                        
+                    </li>
+                    <li>
+                        <a href="">Inventaire</a>
+                        <!-- Sous menu de inventaire -->
+                        <ul>
+                            <li><a href="">Stock/Réaprovisionnement</a></li>
+                            <li><a href="">Ajout d'article</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="">Comptabilité</a>
+                        <!-- Sous menu de Comptabilité -->
+                        <ul>
+                            <li><a href="">Historique des ventes</a></li>
+                            <li><a href="">Bilan</a></li>
+                        </ul>
+                    </li>
+                    <li style="padding-bottom: 2.6%">
+                        <a href="">Gestion des comptes</a>
+                    </li>
                 </ul>
                 <!-- Côté droit de la barre de navigation -->
                 <ul class="menuR">
-                    <li style="width : 20px"><a href=""><img src="../Images/cloche.png"></a></li>
-                    <li style="width : 20px"><a href=""><img src="../Images/icone.png"></a></li>
-                    <li style="width : 20px"><a href=""><img src="../Images/drapeau" style="margin-bottom : 25%"></a></li>
+                    <li style="width : 20px"><a href=""><img src="../Content/Images/cloche.png"></a></li>
+                    <li style="width : 20px"><a href=""><img src="../Content/Images/icone.png"></a></li>
+                    <li style="width : 20px"><a href=""><img src="../Content/Images/drapeau" style="margin-bottom : 25%"></a></li>
                 </ul>
             </ul>
         </nav>
     </div>
 </header>
-<!-- Fin header -->
