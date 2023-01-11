2adc2aec287c20260c64a92169c23f2ad2a9d8aa
DariusPorthos <darius.porthos@gmail.com>
2023-01-11 16:23:17 +0100

    version 0.1

--- Tests/test_connexion.php
+++ Tests/test_connexion.php
@@ -2,8 +2,13 @@
 require_once "../Model/Model.php";
 
 $m = Model::getModel();
-if (isset($_POST["identifiant"]) and isset($_POST["mdp"]))
+if (isset($_POST["identifiant"]) and isset($_POST["mdp"])){
     $identifiant = $_POST["identifiant"];
     $mdp = $_POST["mdp"];
-    if ($m->connexion($identifiant, $mdp))
-        header("Location:test_connexion.php");
+    if ($m->connexion($identifiant, $mdp)){
+        header("Location:connexion_reussie.php");
+    }else{
+        echo "erreur boucle 2";
+    }
+}else
+    echo "erreur boucle 1";
