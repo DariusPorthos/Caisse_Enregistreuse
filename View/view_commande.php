5da10b1cd595cb30bf7cf2ad1a298aa8d2b8f5b8
aline <lyaline.vong@gmail.com>
2023-01-11 16:51:44 +0100

    ....;

--- View/view_commande.php
+++ View/view_commande.php
@@ -11,66 +11,361 @@
 
 <?php require_once "view_begin_client.php" ?>
 
-<section class="step-container">
-        <!-- bloc article 1 start -->
-        <div class="step">
-            <img src="" alt="article1"/>
-            <span>01</span>
-            <h3>Article 1</h3>
-            <p>****</p>
-            <input type="submit" value="Ajouter" class="btn">
+
+<div class="commande">
+    <h1>Commande en cours</h1>
+</div>
+
+<div class="container">
+
+    <div class="article">
+        <h3 class="categorie" id="boisson">Boissons</h3>
+
+        <div class="produit">
+            <img src="../Content/Images/cristaline.jpg">
+            <p class="p">Cristaline - 50cl </p>
+            <p><strong>0.50€</strong> </p>
+
+            <label for="q">Quantité: </label>
+            <select id="quantite" name="q" class="qt">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+            </select>
+
+            <button type="button" class="add-to-cart" data-id="432" data-name="Pastilles" data-price="29,00"
+                    data-weight="97" data-url="/produit/pastilles-anti-douleurs/">Ajouter au panier</button>
         </div>
-        <!-- bloc article 1 ends-->
 
-    <!-- bloc article 2 start -->
-    <div class="step">
-        <img src="" alt="article1"/>
-        <span>01</span>
-        <h3>Article 1</h3>
-        <p>****</p>
-        <input type="submit" value="Ajouter" class="btn">
-    </div>
-    <!-- bloc article 2 ends -->
+        <div class="column"><hr></div>
 
-    <!-- bloc article 3 start -->
-    <div class="step">
-        <img src="" alt="article1"/>
-        <span>01</span>
-        <h3>Article 1</h3>
-        <p>****</p>
-        <input type="submit" value="Ajouter" class="btn">
-    </div>
-    <!-- bloc article 3 ends-->
+        <div class="produit">
+            <img src="../Content/Images/caprisun.webp">
+            <p class="p">Capri Sun - 200ml</p>
+            <p><strong>0.50€</strong> </p>
 
-    <!-- bloc article 4 start -->
-    <div class="step">
-        <img src="" alt="article1"/>
-        <span>01</span>
-        <h3>Article 1</h3>
-        <p>****</p>
-        <input type="submit" value="Ajouter" class="btn">
-    </div>
-    <!-- bloc article 4 ends -->
+            <label for="q">Quantité: </label>
+            <select id="quantite" name="q" class="qt">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+            </select>
 
-    <!-- bloc article 5 start -->
-    <div class="step">
-        <img src="" alt="article1"/>
-        <span>01</span>
-        <h3>Article 1</h3>
-        <p>****</p>
-        <input type="submit" value="Ajouter" class="btn">
-    </div>
-    <!-- bloc article 5 ends-->
+            <button type="button" class="add-to-cart" data-id="432" data-name="Pastilles" data-price="29,00"
+                    data-weight="97" data-url="/produit/pastilles-anti-douleurs/">Ajouter au panier</button>
+        </div>
 
-    <!-- bloc article 6 start -->
-    <div class="step">
-        <img src="" alt="article1"/>
-        <span>01</span>
-        <h3>Article 1</h3>
-        <p>**********</p>
-        <input type="submit" value="Ajouter" class="btn">
+        <div class="column"><hr></div>
+
+        <div class="produit">
+            <img src="../Content/Images/oasis_tropical.jpg">
+            <p class="p">Oasis Tropical - 33cl </p>
+            <p><strong>0.80€</strong> </p>
+
+            <label for="q">Quantité: </label>
+            <select id="quantite" name="q" class="qt">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+            </select>
+
+            <button type="button" class="add-to-cart" data-id="432" data-name="Pastilles" data-price="29,00"
+                    data-weight="97" data-url="/produit/pastilles-anti-douleurs/">Ajouter au panier</button>
+        </div>
+
+        <div class="column"><hr></div>
+
+        <div class="produit">
+            <img src="../Content/Images/oasis_pomme_cassis.jpg">
+            <p class="p">Oasis Pomme Cassis - 33cl </p>
+            <p><strong>0.80€</strong> </p>
+
+            <label for="q">Quantité: </label>
+            <select id="quantite" name="q" class="qt">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+            </select>
+
+            <button type="button" class="add-to-cart" data-id="432" data-name="Pastilles" data-price="29,00"
+                    data-weight="97" data-url="/produit/pastilles-anti-douleurs/">Ajouter au panier</button>
+        </div>
+
+        <div class="column"><hr></div>
+
+        <div class="produit">
+            <img src="../Content/Images/ice_tea.jpg">
+            <p class="p">Lipton Ice Tea - 33cl </p>
+            <p><strong>0.80€</strong> </p>
+
+            <label for="q">Quantité: </label>
+            <select id="quantite" name="q" class="qt">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+            </select>
+
+            <button type="button" class="add-to-cart" data-id="432" data-name="Pastilles" data-price="29,00"
+                    data-weight="97" data-url="/produit/pastilles-anti-douleurs/">Ajouter au panier</button>
+        </div>
+
+        <div class="column"><hr></div>
+
+        <h3 class="categorie" id="soda"> Soda </h3>
+
+        <div class="produit">
+            <img src="../Content/Images/coca.jpg">
+            <p class="p">Coca Cola - 33cl </p>
+            <p><strong>0.80€</strong> </p>
+
+            <label for="q">Quantité: </label>
+            <select id="quantite" name="q" class="qt">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+            </select>
+
+            <button type="button" class="add-to-cart" data-id="432" data-name="Pastilles" data-price="29,00"
+                    data-weight="97" data-url="/produit/pastilles-anti-douleurs/">Ajouter au panier</button>
+        </div>
+
+        <div class="column"><hr></div>
+
+        <div class="produit">
+            <img src="../Content/Images/coca_cherry.png">
+            <p class="p">Coca Cola Cherry - 33cl</p>
+            <p><strong>0.80€</strong> </p>
+
+            <label for="q">Quantité: </label>
+            <select id="quantite" name="q" class="qt">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+            </select>
+
+            <button type="button" class="add-to-cart" data-id="432" data-name="Pastilles" data-price="29,00"
+                    data-weight="97" data-url="/produit/pastilles-anti-douleurs/">Ajouter au panier</button>
+        </div>
+
+        <div class="column"><hr></div>
+
+        <div class="produit">
+            <img src="../Content/Images/fanta.webp.crdownload">
+            <p class="p">Fanta Citron - 33cl </p>
+            <p><strong>0.80€</strong> </p>
+
+            <label for="q">Quantité: </label>
+            <select id="quantite" name="q" class="qt">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+            </select>
+
+            <button type="button" class="add-to-cart" data-id="432" data-name="Pastilles" data-price="29,00"
+                    data-weight="97" data-url="/produit/pastilles-anti-douleurs/">Ajouter au panier</button>
+        </div>
+
+        <div class="column"><hr></div>
+
+        <h3 class="categorie" id="sirop">Eau + Sirop</h3>
+
+        <div class="produit">
+            <img src="../Content/Images/fraise.jpg">
+            <p class="p">Fraise - 50cl </p>
+            <p><strong>0.80€</strong> </p>
+
+            <label for="q">Quantité: </label>
+            <select id="quantite" name="q" class="qt">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+            </select>
+
+            <button type="button" class="add-to-cart" data-id="432" data-name="Pastilles" data-price="29,00"
+                    data-weight="97" data-url="/produit/pastilles-anti-douleurs/">Ajouter au panier</button>
+        </div>
+
+        <div class="column"><hr></div>
+
+        <div class="produit">
+            <img src="../Content/Images/peche.webp">
+            <p class="p">Pêche - 50cl </p>
+            <p><strong>0.80€</strong> </p>
+
+            <label for="q">Quantité: </label>
+            <select id="quantite" name="q" class="qt">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+            </select>
+
+            <button type="button" class="add-to-cart" data-id="432" data-name="Pastilles" data-price="29,00"
+                    data-weight="97" data-url="/produit/pastilles-anti-douleurs/">Ajouter au panier</button>
+        </div>
+
+        <div class="column"><hr></div>
+
+        <div class="produit">
+            <img src="../Content/Images/cassis.jpg">
+            <p class="p">Cassis - 50cl </p>
+            <p><strong>0.80€</strong> </p>
+
+            <label for="q">Quantité: </label>
+            <select id="quantite" name="q" class="qt">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+            </select>
+
+            <button type="button" class="add-to-cart" data-id="432" data-name="Pastilles" data-price="29,00"
+                    data-weight="97" data-url="/produit/pastilles-anti-douleurs/">Ajouter au panier</button>
+        </div>
+
+        <div class="column"><hr></div>
+
+        <div class="produit">
+            <img src="../Content/Images/pomme.jpg">
+            <p class="p">Pomme Verte - 50cl </p>
+            <p><strong>0.80€</strong> </p>
+
+            <label for="q">Quantité: </label>
+            <select id="quantite" name="q" class="qt">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+            </select>
+
+            <button type="button" class="add-to-cart" data-id="432" data-name="Pastilles" data-price="29,00"
+                    data-weight="97" data-url="/produit/pastilles-anti-douleurs/">Ajouter au panier</button>
+        </div>
+
+        <div class="column"><hr></div>
+
+        <div class="produit">
+            <img src="../Content/Images/fruit_passion.png">
+            <p class="p">Fruit de la passion - 50cl </p>
+            <p><strong>0.80€</strong> </p>
+
+            <label for="q">Quantité: </label>
+            <select id="quantite" name="q" class="qt">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+            </select>
+
+            <button type="button" class="add-to-cart" data-id="432" data-name="Pastilles" data-price="29,00"
+                    data-weight="97" data-url="/produit/pastilles-anti-douleurs/">Ajouter au panier</button>
+        </div>
+
+        <div class="column"><hr></div>
+
+
+
     </div>
-    <!-- bloc article 6 ends-->
+
+    <div class="panier">
+        <div class="prix1">
+            <p class="n"></p>
+
+        </div>
+        <div>
+            <p>Produit  &ensp;2</p>
+
+        </div>
+        <div>
+            <p>Produit &ensp; 3</p>
+
+        </div>
+        <div>
+            <p>Produit &ensp; 4</p>
+
+        </div>
+        <hr>
+        <div class="total">
+            <p>Total</p>
+            <p>Prix total &euro;</p>
+        </div>
+    </div>
+
+</div>
 
 </section>
 
