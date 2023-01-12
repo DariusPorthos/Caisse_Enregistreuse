c77b6261b467299756864f7df97d90a7ef7b6686
Jlawl1et <99320146+Jlawl1et@users.noreply.github.com>
2023-01-13 00:49:00 +0100

    Update Controller_compte.php
    ajout de action supprimer compte

--- Controller/Controller_compte.php
+++ Controller/Controller_compte.php
@@ -15,6 +15,38 @@ class Controller_compte extends Controller{
     public function action_creationCompte(){
 
     }
+    
+        public function action_supprimerCompte(){
+        if (isset($_GET["id_utilisateur"]) and preg_match("/^[1-9]\d*$/", $_GET["id_utilisateur"])) {
+            $id = $_GET["id_utilisateur"];
+
+            $m = Model::getModel();
+            $suppression = $m->supprimerCompte($id);
+            if ($suppression) {
+                $message = "Le compte a bien été supprimé .";
+            } else {
+                $message = "Il y a aucun compte avec cet id " . $id . "!";
+            }
+        } else {
+            $message = " Il n'y a pas d'id présent dans l'URL";
+        }
+
+        $data = [
+            "title" => "Suppression de compte.",
+            "message" => $message,
+        ];
+        $this->render("message", $data);
+
+    }
+
+    public function action_list()
+    {
+        $m = Model::getModel();
+        $data = [
+            "liste" => $m->getLast(),
+        ];
+        $this->render("last", $data);
+    }
 
     public function action_default()
     {
