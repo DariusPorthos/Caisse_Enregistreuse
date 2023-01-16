3b08473f243437cd4c13b046e87342055a34ef99
JeffersonPaillot <122374217+JeffersonPaillot@users.noreply.github.com>
2023-01-16 14:06:30 +0100

    Update Test_Article.php
    ça marche !!!!!

--- Tests/Test_Article.php
+++ Tests/Test_Article.php
@@ -8,45 +8,45 @@ $prenom = $m->getPrenom($id);
 ?>
 
 
-<!Doctype html>
-<head>
-    <title>Test Article</title>
-    <meta charset="utf8">
-    <link href="styles/style.css" rel="stylesheet" type="text/css">
-</head>
-<body>
+    <!Doctype html>
+    <head>
+        <title>Test Article</title>
+        <meta charset="utf8">
+        <link href="styles/style.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
 
-<h1>Bonjour <?php echo($role)?><?php echo($nom)?><?php echo($prenom)?></h1>
+    <h1>Bonjour <?php echo($role)?><?php echo($nom)?><?php echo($prenom)?></h1>
 
-<form action="" method="get">
-    <label>Formulaire de modification d'un article</label><br>
-    <label>Id de l'article à modifier : <input type="text" name="ida"></label><br>
-    <label>Nouveau nom de l'article: <input type="text" name="nom"></label><br>
-    <label>Nouveau prix : <input type="text" name="prix"></label><br>
-    <label>Nouvelles infos :<br> <textarea  name="info"></textarea> </label><br>
+    <form action="" method="get">
+        <label>Formulaire de modification d'un article</label><br>
+        <label>Id de l'article à modifier : <input type="text" name="ida"></label><br>
+        <label>Nouveau nom de l'article: <input type="text" name="nom"></label><br>
+        <label>Nouveau prix : <input type="text" name="prix"></label><br>
+        <label>Nouvelles infos :<br> <textarea  name="informations"></textarea> </label><br>
 
-    <label for="pet-select">Nouvelle catégorie :</label>
-    <br>
-    <br>
-    <select name="cate">
-        <option value="">- Choisissez une catégorie -</option>
-        <option value="snack">Snack</option>
-        <option value="boisson">Boisson</option>
-    </select>
-    <br>
-    <br>
-    <label>Nouveau Stock : <input type="number" name="stock" min="1" max="10000"></label>
-    <br>
-    <input type="submit">
-</form>
+        <label for="selection">Nouvelle catégorie :</label>
+        <br>
+        <br>
+        <select name="categorie">
+            <option value="">- Choisissez une catégorie -</option>
+            <option value="snack">Snack</option>
+            <option value="boisson">Boisson</option>
+        </select>
+        <br>
+        <br>
+        <label>Nouveau Stock : <input type="number" name="nbArticle" min="1" max="10000"></label>
+        <br>
+        <input type="submit">
+    </form>
 
 
-</body>
+    </body>
 
 <?php
 $m->setNomArticle($_GET["ida"], $_GET["nom"]);
 $m->setPrix($_GET["ida"], $_GET["prix"]);
-$m->setCategorie($_GET["ida"], $_GET["cate"]);
-$m->setInformations($_GET["ida"], $_GET["info"]);
-$m->setNbArticle($_GET["ida"], $_GET["stock"]);
+$m->setCategorie($_GET["ida"], $_GET["categorie"]);
+$m->setInformations($_GET["ida"], $_GET["informations"]);
+$m->setNbArticle($_GET["ida"], $_GET["nbArticle"]);
 ?>
