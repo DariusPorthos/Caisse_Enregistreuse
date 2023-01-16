e2abd056cd9145d6c1e2070c4f5a6443e7f62864
DariusPorthos <darius.porthos@gmail.com>
2023-01-16 01:20:17 +0100

    ajout de message de bonjour

--- View/view_accueil_client.php
+++ View/view_accueil_client.php
@@ -5,7 +5,7 @@ require "view_header_client.php";
 </header>
 <section class="accueil" id="#accueil">
     <div class="content">
-        <span>Bienvenue</span>
+        <span>Bienvenue,</span><span style="padding-left: 40px"><?php echo $_SESSION["prenom"]?></span>
         <h3></h3>
     </div>
 </section>
